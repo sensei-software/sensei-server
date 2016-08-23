92b16e466a6ebb7329c00e5fcd31c5b003c2d890
daniele <daniele.bruneo@gmail.com>
2016-08-24 00:51:46 +0200

    -

--- res/db/run/purge_invalid.sql
+++ res/db/run/purge_invalid.sql
@@ -1,11 +1,3 @@
-DELETE FROM sensors_log
-	 WHERE EventTime > (NOW() - INTERVAL 90 SECOND)+0
-	 AND(
-		IFNULL(SensorValue,'') = ''
-		OR IFNULL(SensorName,'') = ''
-		
-	);
-
 DELETE FROM sensors_values
 	 WHERE DateField > (NOW() - INTERVAL 90 SECOND)+0
 	 AND(
@@ -14,4 +6,5 @@ DELETE FROM sensors_values
 		OR Measure LIKE  '% %'
 		OR IFNULL(SensorName,'') = ''
 		OR SensorName LIKE  '% %'
+		OR SensorName LIKE  '>%'
 	);
